b0b0a45692dcb6baef0b2e2254c91a7cf478cf4c
sasnigdho <salmanahmedims@gmail.com>
2023-06-18 22:34:29 +0600

    SplashController update to page navigate.

--- lib/app/controllers/splash_controller.dart
+++ lib/app/controllers/splash_controller.dart
@@ -1,7 +1,14 @@
-
 import 'package:get/get.dart';
 
-class SplashController extends GetxController {
+import '../routes/route_names.dart';
 
+class SplashController extends GetxController {
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+
+    await Future.delayed(const Duration(seconds: 2), () async {
+      await Get.offAllNamed(RouteNames.home);
+    });
+  }
 }
-  

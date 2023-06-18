fc5e80bb43617ca150c71fc859ec1c6b07bff2f8
sasnigdho <salmanahmedims@gmail.com>
2023-06-18 23:18:34 +0600

    BottomNavigationBar added in home page.

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,3 +1,7 @@
+import 'package:get/get.dart';
+
 import 'base_controller.dart';
 
-class HomeController extends BaseController {}
+class HomeController extends BaseController {
+  final index = 0.obs;
+}
--- lib/app/ui/pages/home_page/home_page.dart
+++ lib/app/ui/pages/home_page/home_page.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_products_view/app/ui/pages/home_page/tabs/cart_tab.dart';
 import 'package:get/get.dart';
 import '../../../controllers/home_controller.dart';
+import 'tabs/products_tab.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> widgetOptions = <Widget>[
+      const ProductsTab(),
+      const CartTab(),
+    ];
+
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: const SafeArea(
-        child: Text('HomeController'),
+      body: Obx(() => widgetOptions[controller.index.value]),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Products'),
+            BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Cart'),
+          ],
+          currentIndex: controller.index.value,
+          onTap: _onItemTapped,
+        ),
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    controller.index.value = index;
   }
 }

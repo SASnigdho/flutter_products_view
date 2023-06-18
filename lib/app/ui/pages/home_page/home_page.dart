c7968558f46121f6b0593b18736ee371948b185d
sasnigdho <salmanahmedims@gmail.com>
2023-06-18 22:21:50 +0600

    Home page added.

---
+++ lib/app/ui/pages/home_page/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../controllers/home_controller.dart';
+
+class HomePage extends GetView<HomeController> {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: const SafeArea(
+        child: Text('HomeController'),
+      ),
+    );
+  }
+}

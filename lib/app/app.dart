b37c1e74661dfe3e1d930d7ef56c4bfa1927ddd6
sasnigdho <salmanahmedims@gmail.com>
2023-06-18 22:34:08 +0600

    App theme added.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'routes/app_pages.dart';
+import 'routes/route_names.dart';
+import 'ui/theme/light.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      // home: const HomePage(),
+    return GetMaterialApp(
+      title: 'Products View Demo',
+
+      // Routing.
+      initialRoute: RouteNames.splash,
+      getPages: AppPages.pages,
+      builder: (_, child) => child!,
+      unknownRoute: AppPages.pages.first,
+
+      theme: AppTheme.light,
     );
   }
 }

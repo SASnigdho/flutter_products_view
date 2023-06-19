f04e2654347aeed0ac1300b712ea1ae9f9365ed2
sasnigdho <salmanahmedims@gmail.com>
2023-06-19 22:40:14 +0600

    Products list page added.

--- lib/app/ui/pages/home_page/tabs/products_tab.dart
+++ lib/app/ui/pages/home_page/tabs/products_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_products_view/app/const/app_colors.dart';
 import 'package:flutter_products_view/app/controllers/product_controller.dart';
 import 'package:get/get.dart';
 
@@ -7,8 +8,48 @@ class ProductsTab extends GetView<ProductController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('ProductsTab'),
+    return ListView.separated(
+      // shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Card(
+          child: InkWell(
+            borderRadius: BorderRadius.circular(10),
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 60,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: AppColors.primary,
+                    ),
+                    margin: const EdgeInsets.only(right: 16),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Product Name Product Name Product Name Product Name',
+                          style: Get.textTheme.titleLarge,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text('Product Name', style: Get.textTheme.labelSmall),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(),
+      itemCount: 100,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
     );
   }
 }

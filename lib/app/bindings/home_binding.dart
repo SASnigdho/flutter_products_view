09e533f3507a6ed3a3d47b568ebeecd803339489
sasnigdho <salmanahmedims@gmail.com>
2023-06-19 22:57:20 +0600

    Product and cart controller registered.

--- lib/app/bindings/home_binding.dart
+++ lib/app/bindings/home_binding.dart
@@ -1,9 +1,17 @@
+import 'package:flutter_products_view/app/data/provider/product_repository.dart';
 import 'package:get/get.dart';
+import '../controllers/cart_controller.dart';
 import '../controllers/home_controller.dart';
+import '../controllers/product_controller.dart';
 
 class HomeBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<ProductController>(
+      () => ProductController(ProductRepository()),
+    );
+
+    Get.lazyPut<CartController>(() => CartController());
   }
 }

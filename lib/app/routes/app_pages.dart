a4180b843663ad610c59fd0a714e53458dd7970d
sasnigdho <salmanahmedims@gmail.com>
2023-06-19 22:44:52 +0600

    ProductShowPage registered.

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_products_view/app/ui/pages/product_show_page/product_show_page.dart';
 import 'package:get/get.dart';
 
 import '../bindings/home_binding.dart';
+import '../bindings/product_show_binding.dart';
 import '../bindings/splash_binding.dart';
 import '../ui/pages/home_page/home_page.dart';
 import '../ui/pages/splash_page/splash_page.dart';
@@ -27,6 +29,12 @@ class AppPages {
       name: RouteNames.home,
       binding: HomeBinding(),
       page: () => const HomePage(),
+      transition: _defaultTransition,
+    ),
+    GetPage(
+      name: RouteNames.productShow,
+      binding: ProductShowBinding(),
+      page: () => const ProductShowPage(),
       transition: _defaultTransition,
     ),
   ];

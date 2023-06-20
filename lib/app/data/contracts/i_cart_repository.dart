5c3bb275b91fc6675e87c59588d9de1d68f6f073
sasnigdho <salmanahmedims@gmail.com>
2023-06-20 13:06:34 +0600

    ICartRepository added.

---
+++ lib/app/data/contracts/i_cart_repository.dart
@@ -0,0 +1,8 @@
+import '../models/cart/cart.dart';
+
+abstract class ICartRepository {
+  Future<List<Cart>> getCarts();
+  Future<void> save(Cart task);
+  Future<Cart?> getCart(int id);
+  Future<void> delete(Cart task);
+}

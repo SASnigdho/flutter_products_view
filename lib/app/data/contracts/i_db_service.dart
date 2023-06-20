5fb0b7e4287e93a3d1d91ee190e2d627d802b0fd
sasnigdho <salmanahmedims@gmail.com>
2023-06-20 13:00:09 +0600

    IDbService added.

--- lib/app/data/contracts/i_db_service.dart
+++ lib/app/data/contracts/i_db_service.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_products_view/app/data/models/cart/cart.dart';
 
 abstract class IDbService {
-  Future<List<Cart>> getCart();
+  Future<List<Cart>> getCarts();
   Future<void> save(Cart cart);
-  Future<Cart?> get(int id);
+  Future<Cart?> getCart(int id);
   Future<void> delete(Cart task);
 }
